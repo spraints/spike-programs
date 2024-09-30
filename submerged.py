4c0e77199c6688d4cb54efa0b4177504d6a65e16
Matt Burke <spraints@gmail.com>
2024-09-29 23:03:19 -0400

    Sketch robot drive methods

---
+++ submerged.py
@@ -0,0 +1,51 @@
+import runloop
+import motor_pair
+from hub import port, device_uuid, motion_sensor
+
+LARGE_BLUE_WHEEL = 87 # mm
+
+async def main():
+    ace = AdvancedBase()
+    ace.debug()
+    await runloop.sleep_ms(1000)
+    ace.debug()
+
+    # draw an H
+    await ace.drive_forward(80)
+    await ace.show_state()
+    await ace.drive_backward(40)
+    await ace.turn_right(90)
+    await ace.drive_forward(40)
+    await ace.turn_left(90)
+    await ace.drive_forward(40)
+    await ace.drive_backward(80)
+
+    await runloop.sleep_ms(1000)
+    ace.debug()
+
+class AdvancedBase:
+    def __init__(self, leftmotor=port.A, rightmotor=port.B, motorpair=motor_pair.PAIR_1, wheeldiameter_mm=LARGE_BLUE_WHEEL):
+        self.mp = motor_pair.pair(motorpair, leftmotor, rightmotor)
+        self.wd = wheeldiameter_mm
+        self.angle_goal = 0
+        motion_sensor.reset_yaw(0)
+
+    async def drive_forward(self, cm):
+        print("todo: drive forward {} cm".format(cm))
+
+    async def drive_backward(self, cm):
+        print("todo: drive backward {} cm".format(cm))
+
+    async def turn_left(self, degrees):
+        print("todo: turn left {} degrees".format(degrees))
+
+    async def turn_right(self, degrees):
+        print("todo: turn right {} degrees".format(degrees))
+
+    def debug(self):
+        print("[{}] current angle: {}, goal: {}".format(device_uuid(), self.angle_goal, self.get_yaw()))
+
+    def get_yaw(self):
+        return motion_sensor.tilt_angles()[0] / 10
+
+runloop.run(main())
